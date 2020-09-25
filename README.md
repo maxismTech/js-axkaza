9fb56e9544fb74ce003d4b17ebd22b0d3bc5b601
maxismTech <maxim16132@gmail.com>
2020-09-25 20:45:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-axkaza
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-axkaza)
---
+++ index.js
@@ -0,0 +1,55 @@
+
+let employees = [
+  {
+		name: 'Bob',
+    department: 'Accounting',
+    salary: 5000
+  },
+  {
+		name: 'John',
+    department: 'Accounting',
+    salary: 7000
+  },
+  {
+		name: 'Paul',
+    department: 'Accounting',
+    salary: 5070
+  },
+  {
+		name: 'Smart Bob',
+    department: 'IT',
+    salary: 15000
+  },
+  {
+		name: 'AnotherBob',
+    department: 'Accounting',
+    salary: 6000
+  },
+  {
+		name: 'Alice',
+    department: 'IT',
+    salary: 7000
+  },
+  {
+		name: 'George',
+    department: 'Sales',
+    salary: 5000
+  },
+  {
+		name: 'Another George',
+    department: 'Sales',
+    salary: 10000
+  }
+  
+];
+  let prop;
+ function lookUpProfile() {
+   for(x = 0; x < employees.length; x++) {
+     prop === employees[x].name; 
+     return prop;
+  }
+    lookUpProfile();
+
+
+
+ }
